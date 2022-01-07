5759c3b9148cb28fd8b93618c532f9166e5ba120
Sreejith Pazhampilly <37782322+Sreejithpin@users.noreply.github.com>
2022-01-06 23:49:32 -0800

    testing 1

--- lib/Class1.cs
+++ lib/Class1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using System.Text;
 
 namespace lib
 {
@@ -10,9 +13,40 @@ namespace lib
             Console.WriteLine("Hello world!");
         }
 
-        public string UseJsonNetForSomeReason<T>(T input)
+        public async string UseJsonNetForSomeReason<T>(T input) 
         {
-            return JsonConvert.SerializeObject(input);
+            var str = JsonConvert.SerializeObject(input);
+
+            var a = JsonConvert.DeserializeObject(str) ;
+
+            var client = new HttpClient();
+            client.BaseAddress = "";
+
+            var request = new HttpRequestMessage(new HttpMethod("POST"), uri);
+            request.Content = new StringContent(content: string, encoding: Encoding.UTF8, mediaType: "application/json");
+
+            using(HttpResponseMessage response = await client.SendAsync(rquest))
+            {
+                string str1 = await response.Content.ReadAsStringAsync();
+            }
+
+            return str;
+        }
+
+        public void Testing()
+        {
+            var data = JsonConvert.DeserializeObject( System.IO.File.ReadAllText("path")
+            using(var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var str = client.UploadString("addr", "data");
+
+            }
+        }
+
+        public int Add(int i, int j)
+        {
+            return i + j;
         }
     }
 }
