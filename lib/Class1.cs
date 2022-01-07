d4053c5648888364a5cb948961d42e83c23763e5
Sreejith Pazhampilly <37782322+Sreejithpin@users.noreply.github.com>
2022-01-07 00:19:26 -0800

    test

--- lib/Class1.cs
+++ lib/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace lib
@@ -12,7 +13,30 @@ namespace lib
 
         public string UseJsonNetForSomeReason<T>(T input)
         {
+            Test();
             return JsonConvert.SerializeObject(input);
         }
+
+        public void Test()
+        {
+            Test test = new Test() { Id = 1, Name = "t" };
+            Test test1 = new Test() { Id = 2, Name = "t2" };
+            var lst = new List<Test>();
+            lst.Add(test);
+            lst.Add(test1);
+
+            var str = JsonConvert.SerializeObject(lst);
+
+            var t = JsonConvert.DeserializeObject<List<Test>>(str)  ;
+        }
+    }
+ 
+    public class Test
+    {
+        
+        public int Id { get; set; }
+
+ 
+        public string Name { get; set; }
     }
 }
